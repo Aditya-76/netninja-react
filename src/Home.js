3723e2f203b8bd35dd0dc82ee3c694fa9399b17c
Aditya-76 <51781674+Aditya-76@users.noreply.github.com>
2021-02-15 21:12:33 +0530

    map method to display data in array (useState)

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,21 @@ import { useState } from 'react';
 
 const Home = () => {
 
-    const [name, setName] = useState('Aditya');
-
-    const handleClick = () => {
-        setName('Shekhar');
-    };
+    const [blogs, setBlogs] = useState([
+        {title: 'Getting started with react', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Aditya', id:1 },
+        {title: 'What is react', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Mario', id:2 },
+        {title: 'using useState hook', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Kodar', id:3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{ name }</p>
-            <br/>
-            <button onClick={handleClick}>Click Here</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                    <br/>
+                </div>
+            ))}
         </div>
      );
 }
