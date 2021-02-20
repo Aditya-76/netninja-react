46eea5a46a16874652aa51284bb7e5bcc08e4fcb
Aditya-76 <51781674+Aditya-76@users.noreply.github.com>
2021-02-20 15:10:24 +0530

    useEffect hook, promises

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'Getting started with react', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Aditya', id:1 },
-        {title: 'What is react', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Mario', id:2 },
-        {title: 'using useState hook', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'Kodar', id:3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        console.log('I am the function');
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    };
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data);
+            setBlogs(data)
+        })
+    }, [] );
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='ALL BLOGS' handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "Aditya")} title="Aditya's Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title='ALL BLOGS'/>}
         </div>
      );
 }
