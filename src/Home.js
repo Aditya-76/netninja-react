c7a0a2e8e8446c752c29c06752802003c568eeb1
Aditya-76 <51781674+Aditya-76@users.noreply.github.com>
2021-02-20 15:33:57 +0530

    handling and displaying fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,33 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
         .then((res) => {
-            return res.json()
+            if(!res.ok){
+                throw Error("Data Endpoint doesn't exist.")
+            }
+            return res.json();
         })
         .then((data) => {
             console.log(data);
-            setBlogs(data)
-            setIsPending(false)
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
         });
         }, 1000);
     }, [] );
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title='ALL BLOGS'/>}
         </div>
